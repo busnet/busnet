96d3d0496de5cceb51ea3d6e652e927275c609d4
Yehuda Tuvia <yehuda@busnet.co.il>
2015-04-08 23:42:28 +0300

    setting up v1.0.3

---
+++ settings/config.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+    autoPack:true,
+    listen:{
+        host: "localhost",
+	    port: 3002
+    },
+    serve:{
+        host: "localhost",
+        //host: "busnet-v102-env-7nbphvjpan.elasticbeanstalk.com"
+        port: 3002
+    },
+    db:{
+        name:"BusNet",
+        server:"ip-172-31-25-81.us-west-2.compute.internal"
+        //server: "localhost"
+    },
+    wasup:
+    {
+        //url: "localhost:3000/wasup/send",
+        url: "ip-172-31-18-6.us-west-2.compute.internal:3000/wasup/send"
+    }
+}
+
