77a9c0acd20644cc34e2079642eeeaec1cdbf43c
Yehuda Tuvia <yehuda@busnet.co.il>
2015-05-31 23:00:24 +0300

    deploying the merge

--- settings/config.js
+++ settings/config.js
@@ -5,14 +5,14 @@ module.exports = {
 	    port: 3002
     },
     serve:{
-        host: "localhost",
-        //host: "busnet-trucknet-env-xeh5yesvtj.elasticbeanstalk.com",
+        //host: "localhost",
+        host: "busnet-trucknet-env-xeh5yesvtj.elasticbeanstalk.com",
         port: 3002
     },
     db:{
-        name:"BusNet",
-        //server:"ip-172-31-25-81.us-west-2.compute.internal"
-        server: "localhost"
+        name:"trucknet",
+        server:"ip-172-31-25-81.us-west-2.compute.internal"
+        //server: "localhost"
     },
     wasup:
     {
