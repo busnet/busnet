e3961913c579d5a6818b0a14e05feee1ec185c6b
shlomicohen007 <https://github.com/busnet/v1.0.git>
2015-07-05 17:18:11 +0300

    fix last commit

--- Client/public/Min/min.js
+++ Client/public/Min/min.js
@@ -11069,27 +11069,25 @@ function register1Continue() {
 
     ng.ws('addUser',company, function (d) {
         if (d) {
-			//add package to user
-			//d_id - user new id
-			
-			
+		
             var date = new Date();
             date.setTime(date.getTime() + (48 * 60 * 60 * 1000)); // 48H
 
             $.cookie('username', d.username, { path: '/' ,expires: date  });
             $.cookie('h', d.hash, { path: '/', expires: date });
             $.cookie('name', d.dtl.companyName, { path: '/', expires: date });
+			
             $("#regOk").popup("open");
-        }
-        else
-            alert('אנא מלאו ח.פ/עוסק מורשה חוקי');
+			
+			} else {
+				alert('אנא מלאו אחר ההוראות');
+			}
     });
-	} else {
-	alert('אנא מלאו אחר ההוראות');
-	}
 	
+			} else {
+				alert('אנא מלאו ח.פ/עוסק מורשה חוקי');
+			}
 }
-
 
 
 
@@ -11228,7 +11226,6 @@ $( document ).ready(function() {
 		$(".trems").removeClass("ui-disabled");	
 		});
 });
-
 
 function regFinish() {
 	window.close();
