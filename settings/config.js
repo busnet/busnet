beba00834ea09b6e534e9c3f8923f80219cc3ea1
Yehuda Tuvia <yehuda@busnet.co.il>
2015-08-26 20:53:58 +0300

    support for ios push notification

--- settings/config.js
+++ settings/config.js
@@ -5,15 +5,15 @@ module.exports = {
 	    port: 3002
     },
     serve:{
-        //host: "localhost",
-        host: "app.busnet.co.il",
-        port: 80
-        //port: 3002
+        host: "localhost",
+        //host: "app.busnet.co.il",
+        //port: 80
+        port: 3002
     },
     db:{
         name:"BusNet",
-        server:"ip-172-31-25-81.us-west-2.compute.internal"
-        //server: "localhost"
+        //server:"ip-172-31-25-81.us-west-2.compute.internal"
+        server: "localhost"
     },
     wasup:
     {
